22bacc756fe114bdde6c4c4dc419fc3ebf3b1d5d
swarthi <yadu.jain@travelyaari.com>
2015-03-13 17:24:31 +0530

    Corrected jobs.py

--- jobs.py
+++ jobs.py
@@ -1,7 +1,6 @@
 #Author: Heera
 #Date: 2014-09-01
 #Description: helpers for creating jobs
-
 import multiprocessing as mp
 import Queue
 from multiprocessing.managers import SyncManager
@@ -10,9 +9,8 @@ import time
 import pickle
 AUTHKEY= "60c05c632a2822a0a877c7e991602543"
 PORTNUM = 8004 #Preffered port
-IP="10.66.60.90"#'127.0.0.1'
-
-
+IP='127.0.0.1'#"10.66.60.90"
+from collections import OrderedDict
 
 class JobsWaiter(object):	
 	# FAILED 	=-1
@@ -25,13 +23,12 @@ class JobsWaiter(object):
 		manager=JobsManager() #make_client_manager(IP,PORTNUM,AUTHKEY)
 		# status_obj=manager.get_sync_data()
 		req=response["request"]
-		#process_id=req[2]["process_id"]
+		# process_id=req[2]["process_id"]
 		waiter=JobsWaiter(manager,kwrd["name"])
 		waiter.done_task(req)
 		del manager
 		del waiter
 
-	
 	def __init__(self,manager,name):		
 		#self.waiter_queue=manager.get_waiter(name)		
 		self.wait_list=manager.get_wait_list(name)		
@@ -39,8 +36,9 @@ class JobsWaiter(object):
 		self.callbacks_dict=None
 		self.tasks=[]		
 		self.manager=manager
-		self.name=name		
-	
+		self.name=name
+		self.to_push=[]		
+
 	def get_callback_job(self):
 		return ("helpers.jobs","JobsWaiter.callback",{"name":self.name})
 
@@ -49,16 +47,28 @@ class JobsWaiter(object):
 							callback_list=[self.get_callback_job()])	
 		#self.tasks.append(obj)
 		self.wait_list[pickle.dumps(obj)]=JobsWaiter.ADDED
-		
-	def add_job(self,obj):
+
+	def add_job(self,obj,push=False):
+		## Add job to server callbacks
+		if push==True:
+			self.__push_task__(obj)
+		else:
+			self.to_push.append(obj)
+
+	def done_task(self,obj):		
+		del self.wait_list[pickle.dumps(obj)]
+
+	def __push_task__(self,obj):
 		## Add job to server callbacks
 		job=obj
 		callback_list=[self.get_callback_job()]
-		
+
 		if self.job_q is None:
 			self.job_q=self.manager.get_job_q()
+
 		if self.callbacks_dict is None:
 			self.callbacks_dict=self.manager.get_callbacks_dict()		
+
 		if type(job)==tuple:
 			if len(job)==2:
 				job.add({})			
@@ -66,26 +76,31 @@ class JobsWaiter(object):
 				raise Exception("Invalid job !")
 		else:
 			raise Exception("Invalid job !")
+
+		job=(job[0],job[1],OrderedDict(sorted(job[2].items())))
 		self.job_q.put(job)				
-		req=job		
+		req=job
 		for callback_job in  callback_list:
-			if type(req)==tuple:				
-				if len(req)==2:
-					req.add({})			
+			if type(job)==tuple:				
+				if len(job)==2:
+					job.add({})			
 				if len(callback_job)==2:
 					callback_job.add({})			
 				if len(callback_job)==0:
 					raise Exception("Invalid callbacks !")
 				else:
-					self.callbacks_dict[pickle.dumps(req)]=callback_job
-					req=callback_job
+					self.callbacks_dict[pickle.dumps(job)]=callback_job
+					job=callback_job
 			else:
 				raise Exception("Invalid callbacks !")
 		## Add job to local wait list
-		self.wait_list[pickle.dumps(obj)]=JobsWaiter.ADDED
-	def done_task(self,obj):
-		#self.waiter_queue.put(obj)		
-		del self.wait_list[pickle.dumps(obj)]
+		self.wait_list[pickle.dumps(req)]=JobsWaiter.ADDED
+
+	def push_all(self):
+		print "pushing tasks %d" %(len(self.to_push))
+		for obj in self.to_push:
+			self.__push_task__(obj)
+		self.to_push=[]
 
 	def wait(self,timeout=6*60*60,finish=True):
 		dt_start=datetime.now()
@@ -93,36 +108,23 @@ class JobsWaiter(object):
 		print "waiting in sink"
 		try:			
 			time.sleep(1)
-			print self.wait_list.keys()
-			for key in self.wait_list.keys():
-				self.wait_list[key]=JobsWaiter.WAITING
-
+			self.push_all()
 			while len(self.wait_list.keys())>0 and curr_timeout>0:
-				
-				# try:
-				# 	obj=self.waiter_queue.get(True,curr_timeout)
-				# except Queue.Empty:
-				# 	break
-				#if obj in self.tasks:
-				#	del self.tasks[self.tasks.index(obj)]
 				diff = datetime.now() - dt_start				
 				curr_timeout = timeout - int(diff.total_seconds())
 				time.sleep(1)
-				print "timeout=%d" % (curr_timeout)
+				print "waiting tasks=%d\ttimeout=%d" % (len(self.wait_list.keys()),curr_timeout)
 			self.manager.delete_waiter(self.name)
-			print self.wait_list.keys()
-			print self.manager.get_wait_list(self.name)		
 		except Exception as e:
 			print e
 
 	def get_tasks(self):
 		return self.tasks
 
-
 ##-----------------------------------New API-------------------------------------###
+
 class JobsManager(SyncManager):
 	"""
-
 	"""
 	def __init__(self):
 		super(JobsManager, self).__init__(address=(IP, PORTNUM), authkey=AUTHKEY)
@@ -138,8 +140,6 @@ JobsManager.register('delete_waiter')
 JobsManager.register('get_wait_list')	
 
 ##-----------------------------------------------------------------------------##
-
-
 
 ## Older API ####
 class JobsConsumer(SyncManager):
@@ -161,10 +161,8 @@ def make_client_manager(ip, port, authkey):
 	JobsConsumer.register('get_callbacks_dict')	
 	JobsConsumer.register('get_waiter')	
 	JobsConsumer.register('get_wait_list')	
-	
 	JobsConsumer.register('delete_waiter')
-	
-	
+
 	manager = JobsConsumer(address=(ip, port), authkey=authkey)
 	manager.connect()
 
